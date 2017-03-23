774e6cfd7df82cbb188b104215543897a9f53084
Pragathi N <pragathi.code@gmail.com>
2017-03-22 23:15:20 -0500

    variables type created

---
+++ variables.py
@@ -0,0 +1,13 @@
+# variables can be initialized without any type mentioned to it
+
+intVar = 10
+floatVar = 1.1
+stringVar = "Variable"
+boolVar = True
+
+#We can know the type of these variables using type()
+
+print(type(intVar))
+print(type(floatVar))
+print(type(stringVar))
+print(type(boolVar))
