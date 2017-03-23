e7bff9e8db9e3bd244f76dfdd4e943a2e386abdb
Pragathi N <pragathi.code@gmail.com>
2017-03-22 23:02:45 -0500

    Basic Operators created

---
+++ basicOperators.py
@@ -0,0 +1,13 @@
+#Addition and Subtraction
+print( 1 + 2)
+print( 5 - 3)
+
+#Multiplication and Division
+print ( 4 * 3)
+print( 4 / 2)
+
+#Exponentiation
+print ( 4 ** 2 )
+
+#Modulo
+print ( 16 % 2)
