54b0ee5416f92f8ecaec5a9d16a3e32e806fc874
Pragathi <pragathi.code@gmail.com>
2017-12-24 01:36:32 -0600

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,15 @@
+def reverse(text):
+  rev = []
+  index = len(text)
+  while index:
+    index -= 1                       
+    rev.append(text[index])
+  return ''.join(rev)
+  
+  
+# Extended Slice
+reverse[::1]
+# esrever
+  
+''.join(reversed('reverse'))
+# esrever
