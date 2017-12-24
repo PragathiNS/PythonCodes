173a3452eb51c577bf85224e7ca1e36b575651eb
Pragathi <pragathi.code@gmail.com>
2017-12-24 02:57:14 -0600

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -46,3 +46,38 @@ def is_int(x):
     else:
         return False
  
+# Statistics
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades_input):
+  for grade in grades_input:
+    print grade
+
+def grades_sum(scores):
+  total = 0
+  for score in scores: 
+    total += score
+  print total
+  return total
+    
+def grades_average(grades_input):
+  sum_of_grades = grades_sum(grades_input)
+  average = sum_of_grades / float(len(grades_input))
+  print average
+  return average
+
+def grades_variance(scores):
+  average = grades_average(scores)
+  variance = 0
+  for score in scores:
+    variance += (average - score) ** 2
+  variance = variance / len(scores)
+  print variance
+  return variance
+
+def grades_std_deviation(variance):
+  return variance ** 0.5
+
+variance = grades_variance(grades)
+print print_grades(grades)
+print grades_std_deviation(variance)
