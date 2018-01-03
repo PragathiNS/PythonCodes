7054c3be05771826ba0a468f2a2a345a79744d67
Pragathi <pragathi.code@gmail.com>
2018-01-03 16:09:16 -0600

    Update car_class.py

--- car_class.py
+++ car_class.py
@@ -1,12 +1,17 @@
 class Car(object):
   condition = "new"
   def __init__(self, model, color, mpg):
+    """Initialize"""
     self.model = model
     self.color = color
     self.mpg   = mpg
     
   def display_car(self):
     print "This is a " + self.color + " " + self.model + " with " + str(self.mpg) + " MPG."
+
+  def __repr__(self):
+    """representation"""
+    return "(%s, %s, %d)" % (self.color, self.model, self.mpg)
 	
   def drive_car(self):
     self.condition = "used"
